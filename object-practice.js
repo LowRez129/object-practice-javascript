2f816b27ff0763e14479158280073c2682c65441
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-08-29 18:36:46 +1200

    Edit object-practice.js

--- object-practice.js
+++ object-practice.js
@@ -1,11 +1,12 @@
 const myLibrary = [];
+let id_counter = 0;
 
-function Book(title, author, pages) {
+function Book(title, author, pages, id) {
     // the constructor...
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.showvalue = () => { console.log(title, author, pages) };
+    this.id = id;
 }
 
 function addBookToLibrary(title, author, pages) {
@@ -13,16 +14,25 @@ function addBookToLibrary(title, author, pages) {
     const library = document.querySelector("#content");
     const div = document.createElement("div");
 
-    let book = new Book(title, author, pages);
+    let book = new Book(title, author, pages, id_counter);
     myLibrary.push(book);
 
+    div.classList.add(id_counter);
     div.append(`${title}, ${author}, ${pages}`);
     library.append(div);
+
+    id_counter += 1;
+}
+
+function remove_book(id) {
+ 
 }
 
 addBookToLibrary("Test", "Ting", "123");
 addBookToLibrary("Aegis", "Noctourniquet", "Track 2");
+remove_book(0);
 
-console.log(myLibrary);
-myLibrary[0].showvalue();
-myLibrary[1].showvalue();
+let tmv = myLibrary[1].id;
+myLibrary.splice(tmv, 1);
+
+console.log(myLibrary, tmv)
