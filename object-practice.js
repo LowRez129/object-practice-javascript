44551fc8287de3b37e4bcf38e5d8fd06c6040e0c
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-08-30 15:07:04 +1200

    Edit All Except(README.md)

--- object-practice.js
+++ object-practice.js
@@ -1,19 +1,19 @@
 const myLibrary = [];
 let id_counter = 0;
 
-function Book(song, album, track, id) {
+function Music(song, album, track, id) {
     this.song = song;
     this.album = album;
     this.track = track;
     this.id = id;
 }
 
-function addBookToLibrary(song, album, track) {
-    const library = document.querySelector("#content");
+function addMusicToLibrary(song, album, track) {
+    const library = document.querySelector(".library");
     const div = document.createElement("div");
 
-    let book = new Book(song, album, track, id_counter);
-    myLibrary.push(book);
+    let music = new Music(song, album, track, id_counter);
+    myLibrary.push(music);
 
     div.setAttribute("id", `${id_counter}`);
     div.append(`${song}, ${album}, ${track}`);
@@ -22,18 +22,18 @@ function addBookToLibrary(song, album, track) {
     id_counter += 1;
 }
 
-function remove_book(id) {
-    const book = document.getElementById(`${id}`);
-    book.remove();
-    myLibrary[id] = Book.prototype;
+function remove_Music(id) {
+    const music = document.getElementById(`${id}`);
+    music.remove();
+    myLibrary[id] = Music.prototype;
 }
 
-addBookToLibrary("Test", "Ting", "123");
-addBookToLibrary("Aegis", "Noctourniquet", "Track 2");
-addBookToLibrary("Eunuch Provocateur", "Tremulant", "Track 3");
-remove_book(0)
-addBookToLibrary("Test", "Ting", "123");
-addBookToLibrary("Roullete Dares", "De-loused in the Comatorium", "Track 2");
-addBookToLibrary("Tourmaline", "Que Dios Te Maldiga Mi Corazon", "Track 11");
+addMusicToLibrary("Test", "Ting", "123");
+addMusicToLibrary("Aegis", "Noctourniquet", "Track 2");
+addMusicToLibrary("Eunuch Provocateur", "Tremulant", "Track 3");
+remove_Music(0)
+addMusicToLibrary("Test", "Ting", "123");
+addMusicToLibrary("Roullete Dares", "De-loused in the Comatorium", "Track 2");
+addMusicToLibrary("Tourmaline", "Que Dios Te Maldiga Mi Corazon", "Track 11");
 
 console.log(myLibrary);
