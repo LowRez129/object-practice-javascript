3c4fc79dd6c44099cad7c2f116e6b689b30b3456
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-09-02 08:57:52 +1200

    Edit [js, css]

--- object-practice.js
+++ object-practice.js
@@ -9,7 +9,12 @@ function Music(song, album, band) {
 function addMusicToLibrary(song, album, band) {
     const library = document.querySelector(".library");
     const div = document.createElement("div");
+    const div_music_container = document.createElement("div");
+    const div_song = document.createElement("div");
+    const div_album = document.createElement("div");
+    const div_band = document.createElement("div");
     const remove = document.createElement("button");
+    const checkbox = document.createElement("input");
 
     let music = new Music(song, album, band);
     myLibrary.push(music);
@@ -21,10 +26,17 @@ function addMusicToLibrary(song, album, band) {
         get_id.remove()
     });
 
+    checkbox.setAttribute("type", "checkbox");
+
     div.setAttribute("music", `${myLibrary.length - 1}`);
     div.id = `${myLibrary.length - 1}`;
+    div_song.append(`${song}`);
+    div_album.append(`${album}`);
+    div_band.append(`${band}`);
+    div_music_container.setAttribute("class", "music-container")
+    div_music_container.append(div_song, div_album, div_band);
+    div.append(div_music_container, checkbox, remove)
     library.append(div);
-    div.append(`${song}, ${album}, ${band}`, remove);
 }
 
 function receiveInput() {
